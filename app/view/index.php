edace2aa8cda45a9111eb2ffba26a5e8cbb854e7
YordyHc <yordyhuamanc@gmail.com>
2025-03-05 15:15:40 -0500

    feat: agregar vista de datos del perfil

--- app/view/index.php
+++ app/view/index.php
@@ -4,13 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <style>
 
     </style>
 </head>
-<div class="container border">
-    <center><h2>hola a todos</h2></center>
-    <p>
+
+<div class="profile-container container">
+        <img src="https://placehold.co/400/orange/white" alt="Perfil" class="profile-pic">
+        
+        <div class="profile-info">
+            <div class="profile-header">
+                <h2>MURAD OSMANN <img src="https://placehold.co/400/blue/white" alt="Verificado" class="verified-icon"></h2>
+                <p>@muradosmann</p>
+            </div>
+            <div class="stats">
+                <span><strong>762</strong> Posts</span>
+                <span><strong>3.2M</strong> Followers</span>
+                <span><strong>1.6K</strong> Following</span>
+            </div>
+        </div>
+
+        <button class="follow-btn">
+            <i class="fab fa-instagram"></i> Follow
+        </button>
+    </div>
 </div>
 <body>
     
