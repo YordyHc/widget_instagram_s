fcf66e3092f5be1310415e47048e071dd08dcf16
YordyHc <yordyhuamanc@gmail.com>
2025-03-05 15:05:51 -0500

    feat: instanciar el modulo css de hoja de estilo

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/bootstrap.min.css" rel="stylesheet"> </link>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/estilos.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
