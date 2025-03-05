333149f6491fa54bf5b20f6c8e88528981528918
YordyHc <yordyhuamanc@gmail.com>
2025-03-05 17:40:16 -0500

    refactor(ui): mejorar estilos y estructura del contenedor de perfil

--- app/view/index.php
+++ app/view/index.php
@@ -4,33 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <style>
 
     </style>
 </head>
-
-<div class="profile-container container">
-        <img src="https://placehold.co/400/orange/white" alt="Perfil" class="profile-pic">
-        
-        <div class="profile-info">
-            <div class="profile-header">
-                <h2>MURAD OSMANN <img src="https://placehold.co/400/blue/white" alt="Verificado" class="verified-icon"></h2>
-                <p>@muradosmann</p>
-            </div>
-            <div class="stats">
-                <span><strong>762</strong> Posts</span>
-                <span><strong>3.2M</strong> Followers</span>
-                <span><strong>1.6K</strong> Following</span>
+<body>
+    <div class="perfil container">
+        <div class="perfil-info"> 
+            <img src="https://placehold.co/400/orange/white" alt="Perfil" class="perfil-img">
+            <div class="perfil-nom">
+                <h2>TITULO PRUEBA <img src="https://placehold.co/400/blue/white" alt="Verificado" class="verificado"></h2>
+                <p>@probando</p>
             </div>
         </div>
-
-        <button class="follow-btn">
+        <div class="estats">
+            <div>
+                <div id="c_posts" class="cant">764</div>
+                <div class="divper">Posts</div>
+            </div>
+            <div>
+                <div id="seguidores" class="cant">3.2M</div>
+                <div class="divper">Followers</div>
+            </div>
+            <div>
+                <div id="seguidos" class="cant">1.6K</div>
+                <div class="divper">Following</div>
+            </div>
+        </div>
+        <button class="boton-seg">
             <i class="fab fa-instagram"></i> Follow
         </button>
     </div>
-</div>
-<body>
-    
 </body>
 </html>
