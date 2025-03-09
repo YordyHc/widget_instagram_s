64428e83c6779483b708d82f658a88ab7c633a7d
YordyHc <yordyhuamanc@gmail.com>
2025-03-09 01:24:49 -0500

    refactor(ui): mejorar la vita de las publicaciones

--- app/view/index.php
+++ app/view/index.php
@@ -68,11 +68,15 @@
                 galeria.innerHTML += `
                     <div class="publicacion">
                         <img src="${post.media_url}" alt="${post.caption || "Publicación de Instagram"}">
-                        <div class="overlay">
+                        <div class="overlay"><br><br><br><br><br><br>
                             <div class="meta-info">
-                                <span>❤️ ${post.like_count || 0} hola</span>
-                                <span>💬 ${post.comments_count || 0}</span>
-                            </div>
+                                <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25">
+                                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" fill="none" stroke="white" stroke-width="2"/>
+                                </svg> ${post.like_count || 0}</span>
+                                <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="25" height="25">
+                                    <path d="M21 15c0 1.1-.9 2-2 2h-5v3l-4-3H5c-1.1 0-2-.9-2-2V7c0-1.1.9-2 2-2h14c1.1 0 2 .9 2 2v8z" fill="none" stroke="white" stroke-width="2"/>
+                                </svg> ${post.comments_count || 0}</span>
+                            </div><br>
                             <p>${post.caption || "Sin descripción"}</p>
                         </div>
                     </div>
