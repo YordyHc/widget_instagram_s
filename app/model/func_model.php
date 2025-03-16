e8ad13eb3802d20c0296fa9543f660aa45d2646d
YordyHc <yordyhuamanc@gmail.com>
2025-03-16 01:54:55 -0500

    fix: usar rutas absolutas para la acceder a al token e id

--- app/model/func_model.php
+++ app/model/func_model.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'config.php';
+require_once __DIR__ . '/../../includes/config.php';
 
 
 class InstaModel {
--- config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define('TOKEN_ACCES', 'ingresa_token_Acceso');
-define('ID_CUENTA', 'ingresa_id');
